f5c6ecf0d5eade95fec42d45867ced9a75a34d1a
salah <salahelsharkawy23@gmail.com>
2024-07-23 22:27:52 +0300

    show time

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+            echo "hello";
+            //add dynamc content
+            echo "<p>Now, its ";
+            echo date('H:i, jS F Y');
+            echo "</p>"
+        ?>
+    </body>
+</html>
