7d999ea17a5ba00e6d5945b8d5ff207cec137e8c
salah <salahelsharkawy23@gmail.com>
2024-07-23 22:53:07 +0300

    add form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <form action="" method="post">
+        <label for="name">Name</label><input type="text" name="name" id="name"> <br>
+        <label for="email">email</label><input type="email" name="email" id="email"> <br>
+        <label for="gender">gender</label><br>
+        <input type="radio" name="gender" value="male">male <br>
+        <input type="radio" name="gender" value="femail">femmale <br>
+        <input type="submit" name="submit" value="register"> <br>
+
+        </form>
+    </body>
+</html>
