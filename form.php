11367ac63862056a58e058c7969e4560301c73b5
salah <salahelsharkawy23@gmail.com>
2024-07-24 18:55:15 +0300

    add process.php that form send data to it

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-        <form action="" method="post">
+        <form action="process.php" method="post">
         <label for="name">Name</label><input type="text" name="name" id="name"> <br>
         <label for="email">email</label><input type="email" name="email" id="email"> <br>
         <label for="gender">gender</label><br>
---
+++ process.php
@@ -0,0 +1,6 @@
+<?php
+    // vardump = print_r = print array
+    var_dump($_POST);
+    echo "<br>";
+    echo $_POST['name']
+?>
